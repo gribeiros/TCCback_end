7a09011b658bdd392c14618efa5f00649b73f78a
Gustavo R. Silva <gustavors071998@gmail.com>
2022-12-10 21:33:32 -0300

    add_logger

--- src/util/Schedules.js
+++ src/util/Schedules.js
@@ -6,12 +6,11 @@ import log4js from "./Logger.js";
 const logger = log4js.getLogger("Schedules");
 
 export function arduinoSocket() {
-  cron.schedule("*/5 * * * * *", async () => {
+  cron.schedule("*/1 * * * * *", async () => {
     try {
       const job = "arduinoStatus";
-      //const response = await axios.get('http://192.168.100.123/S');
-      //const data = response.data;
-      const data = { waterLevel: 450, flowLevel: 0 };
+      const response = await axios.get('http://192.168.100.123/S');
+      const data = response.data;
       logger.info("Executando tarefa " + job + " ");
       logger.debug("Dados: " + JSON.stringify(data));
       await emit(job, { status: data });
